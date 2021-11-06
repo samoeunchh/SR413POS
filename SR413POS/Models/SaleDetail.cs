98b5b29c7edd8645a954dc8399e4cf3a14c5f850
samoeun chhay <samoeunchh@gmail.com>
2021-11-06 15:38:38 +0700

    Add Sale

---
+++ SR413POS/Models/SaleDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SR413POS.Models
+{
+    public class SaleDetail
+    {
+        [Key]
+        public Guid SaleDetailId { get; set; }
+        public Guid SaleId { get; set; }
+        public Guid ProductId { get; set; }
+        public Guid UnitId { get; set; }
+        public double Price { get; set; }
+        public int Qty { get; set; }
+        public double Amount { get; set; }
+        public double Discount { get; set; }
+        public double Total { get; set; }
+    }
+}
