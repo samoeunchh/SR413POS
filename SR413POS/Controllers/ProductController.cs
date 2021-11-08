ee5bef607ffd096acb8def0fe8c5c82bac1bc227
samoeun chhay <samoeunchh@gmail.com>
2021-11-08 14:11:35 +0700

    Scan Item in Sale

---
+++ SR413POS/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SR413POS.Data;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace SR413POS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("{code}")]
+        public async Task<IActionResult> GetProduct(string code)
+        {
+            var data =await (from p in _context.Product
+                        join pp in _context.ProductPrice on p.UnitId equals pp.UnitId
+                        where p.Barcode.Equals(code)
+                        select new
+                        {
+                            p.ProductId,
+                            p.ProductName,
+                            p.UnitId,
+                            pp.Price,
+                            p.Cost,
+                            p.OnHand
+                        }).FirstOrDefaultAsync();
+            return Ok(data);
+        }
+    }
+}
--- SR413POS/wwwroot/js/site.js
+++ SR413POS/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//Get Unit
+var options = "";
+$.ajax({
+    method: "GET",
+    url: "/Products/GetUnits",
+    dataType: "json",
+    success: function (content) {
+        $.each(content, function (i, v) {
+            options += "<option value='" + v.unitId + "'>" + v.unitName + "</option>";
+        });
+    }, error: function (error) {
+        console.log(error);
+    }
+});
